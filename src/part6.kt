405caed44004facc778bd89c19014f90d4300bf2
Yousef <yayalkandari@gmail.com>
2025-02-12 20:20:22 +0300

    removed comments

--- src/part6.kt
+++ src/part6.kt
@@ -1,14 +1,6 @@
-
-//Find and print the largest and smallest numbers using maxOrNull() and minOrNull().
-//Expected Output:
-//Largest number: 9.2
-//Smallest number: 2.5
-
 fun main() {
     var specialNumbers = arrayListOf(2.5, 7.1, 3.8, 9.2, 5.6);
 
     println("Largest number: ${specialNumbers.max()}");
     println("Smallest number: ${specialNumbers.min()}");
-
-
 }
